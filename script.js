3a7f8723a7234ce66dbf0a902f515dbd9c53082c
raesii <leisolo@gmail.com>
2022-01-29 14:14:05 -0800

    fixed bg color and span

--- script.js
+++ script.js
@@ -15,47 +15,10 @@ var currentTime = moment().hour()
     var userplan = localStorage.getItem(i)
     $("#hour-" + i).val(userplan)
     if(i<currentTime) {
-      $("#hour-" + i).addClass("bg-primary")
-    } else if( i == currentTime) {
       $("#hour-" + i).addClass("bg-secondary")
+    } else if( i == currentTime) {
+      $("#hour-" + i).addClass("bg-light")
     }else{
-      $("#hour-" + i).addClass("bg-warning")
+      $("#hour-" + i).addClass("bg-info")
     }
-  }
-// var saveButton = document.getElementsByTagName('button');
-// var eventDetails = document.getElementsByTagName('textarea');
-// 
-
-
-// var d = new Date();
-// var hours = d.getHours();
-
-// var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-//
-
-// saveButton.addEventListener('click', saveEvent)
-
-// function saveEvent() {
-//     var newEvent = JSON.parse(localStorage.getItem('')) || [];
-//     var s = document.getElementsByTagName('textarea').value;
-//     var newEvent = {time, Text:Text}
-//     eventDetails.push(newEvent)
-//     localStorage.setItem('textarea', JSON.stringify(newEvent))
-//  };
-
-
-
-// //*function checkTime() {
-//   //  var d = new Date(); // current time
-//     //var hours = d.getHours();
-//   //  var mins = d.getMinutes();
-//     // if(hours>=23 || hours <=00)
-//       //{
-//         //  document.body.style.background="pink";//set background color x
-//       //}
-//       //else
-//       //{
-//        //    document.body.style.background="blue";//set background color y
-//       //}
-
-// //}
+  };
